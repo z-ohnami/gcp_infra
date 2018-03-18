0ceeac67e84c381ef88eb9509d490f448f342768
Makoto Ohnami <z.ohnami@gmail.com>
2018-03-18 23:10:45 +0900

    domain

---
+++ terraform/gcp_dns.tf
@@ -0,0 +1,43 @@
+resource "google_dns_managed_zone" "wp-dns-zone" {
+  name     = "wp-dns-zone"
+  dns_name = "${file("credentials/dns_name.txt")}"
+}
+
+resource "google_dns_record_set" "wp-dns-record-set" {
+  name = "${google_dns_managed_zone.wp-dns-zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.wp-dns-zone.name}"
+
+  rrdatas = [
+    "${google_compute_global_address.wp-global-address.address}"
+  ]
+}
+
+resource "google_dns_record_set" "wp-dns-record-set-cname" {
+  name = "www.${google_dns_managed_zone.wp-dns-zone.dns_name}"
+  type = "CNAME"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.wp-dns-zone.name}"
+
+  rrdatas = [
+    "${google_dns_record_set.wp-dns-record-set.name}"
+  ]
+}
+
+# resource "google_dns_record_set" "wp-dns-record-set-ns" {
+#   name = "${google_dns_managed_zone.wp-dns-zone.dns_name}"
+#   type = "NS"
+#   ttl  = 300
+
+#   managed_zone = "${google_dns_managed_zone.wp-dns-zone.name}"
+
+#   rrdatas = [
+#     "ns-cloud-b1.googledomains.com.",
+#     "ns-cloud-b2.googledomains.com.",
+#     "ns-cloud-b3.googledomains.com.",
+#     "ns-cloud-b4.googledomains.com."
+#   ]
+# }
